6ebdd9d88a477e202e5e169cc01a2b2fcef4c010
bronifty <bronifty@gmail.com>
2023-05-17 12:12:20 -0400

    useStyles$

--- src/routes/me/beer-selector.tsx
+++ src/routes/me/beer-selector.tsx
@@ -1,10 +1,18 @@
-import { Resource, component$, useResource$ } from "@builder.io/qwik";
+import {
+  Resource,
+  component$,
+  useResource$,
+  useStyles$,
+} from "@builder.io/qwik";
+import styles from "./beer-selector.css?inline";
 
 export interface Beer {
   name: string;
 }
 
 export const BeerSelector = component$(() => {
+  useStyles$(styles);
+
   // const beers: Beer[] = [{ name: "IPA" }, { name: "Lager" }];
   const beersResource = useResource$<Beer[]>(async () => {
     const res = await fetch("https://api.sampleapis.com/beers/ale");
